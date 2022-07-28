75041c2b51edfd6a1a3a2de8fb2d599508738be5
Diana Bayazitova <onlydi@mail.ru>
2022-07-28 20:17:11 +0500

    Added SalesManager.java with reformated code

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -2,7 +2,8 @@ public class SalesManager {
     protected int[] sales;
     protected int min;
     protected int max;
-    protected int sum=0;
+    protected int sum = 0;
+
     public SalesManager(int[] sales) {
         this.sales = sales;
     }
@@ -16,29 +17,29 @@ public class SalesManager {
         }
         return max;
     }
-    public int min() {
-        int min = sales [0];
-        for (int i =0;i<sales.length;i++){
 
-            if (sales[i]<min){
-            min=sales[i];
-        }
-        else {
-        min = sales [0];}
+    public int min() {
+        int min = sales[0];
+        for (int i = 0; i < sales.length; i++) {
+
+            if (sales[i] < min) {
+                min = sales[i];
+            } else {
+                min = sales[0];
+            }
         }
         return min;
     }
-    public int average (){
+
+    public int average() {
         min = min();
         max = max();
-     for (int i=0;i<sales.length;i++){
-         sum+=sales[i];
-     }
-        int generalAverage = (sum-max-min)/(sales.length-2);
-     return generalAverage;
+        for (int i = 0; i < sales.length; i++) {
+            sum += sales[i];
+        }
+        int generalAverage = (sum - max - min) / (sales.length - 2);
+        return generalAverage;
     }
-
-
 
 
 }
